3ff3dbdd99559c4a59a447cc8f318bf483f53d56
panwaraporn <58030219@kmitl.ac.th>
2017-10-28 19:29:23 +0700

    commit Lab04P18

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,40 +1,18 @@
 ﻿
 using System;
-using System.IO;
-public class switchLearning
+
+namespace ConsoleApp2
 {
-    public static void Main()
+    class WhileTest
     {
-        Console.Write("Input day name (sun, mon, tue, wed, thu, fri or sat) : ");
-        string gradeString = Console.ReadLine();
-        string message;
-        switch (gradeString.ToUpper())
+        static void Main()
         {
-            case "sun":
-                message = "sun is Sunday, color Red";
-                break;
-            case "mon":
-                message = "mon is Monday, color Yellow";
-                break;
-            case "tue":
-                message = "tue is Tuesday, color Pink";
-                break;
-            case "wed":
-                message = "wed is Wednesday, color Green";
-                break;
-            case "thu":
-                message = "thu is Thursday, color Orange";
-                break;
-            case "fri":
-                message = "fri is Friday, color Blue";
-                break;
-            case "sat":
-                message = "sat is Saturday, color Purple";
-                break;
-            default:
-                message = "Incorrect days";
-                break;
+            int n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine("Current value of n is {0}", n);
+                n++;
+            }
         }
-        Console.WriteLine(message);
     }
 }
