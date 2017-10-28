186cf60725f5ff6510a7511158152f8fef018cb5
panwaraporn <58030219@kmitl.ac.th>
2017-10-28 17:10:11 +0700

    commit Lab04P02

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -4,11 +4,14 @@ public class GotoTest
     static void Main(string[] args)
     {
         Console.WriteLine("Line 1");
-        Console.WriteLine("Line 2");
-        Console.WriteLine("Line 3");
     line4:
+        Console.WriteLine("Line 2");
+    line9:
+        Console.WriteLine("Line 3");
+    
         Console.WriteLine("Line 4");
         Console.WriteLine("Line 5");
+    line2:
         Console.WriteLine("Line 6");
         goto line10;
         Console.WriteLine("Line 7");
